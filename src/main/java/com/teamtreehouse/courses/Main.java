e9f74197c59e3383584045fec5d6052e01d60977
justinm-nyc <justin.martinelli@oracle.com>
2019-11-13 10:53:42 -0500

    Added new template

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,11 +1,16 @@
 package com.teamtreehouse.courses;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
 
-        get("/", (request, response) -> "Welcome Students!");
+        // index.hbs file is in resources/templates directory
+        get("/", (request, response) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
